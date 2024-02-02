75bad73c8ec13d9dfa06ddca63c3cef26cfbad91
Chetan3327 <chauhanchetan12789@gmail.com>
2024-02-02 17:36:49 +0530

    all user and find user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -62,4 +62,39 @@ const loginUser = async (req, res) => {
     }
 }
 
-export {registerUser, loginUser}
+const allUser = async (req, res) => {
+    try {
+      const users = await UserModel.find({}, { password: 0 }); // Exclude the password field from the response
+      res.status(200).json(users);
+    } catch (error) {
+      console.error('Error fetching all users:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+const findUser = async (req, res) => {
+    console.log(req.query.search)
+    try {
+        const keyword = req.query.search
+        ? {
+            $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+            ],
+        }
+        : {};
+        
+        const users = await UserModel.find({ ...keyword, _id: { $ne: req.user._id } }, { password: 0 });
+
+        if (!users || users.length === 0) {
+            res.status(404).json({ message: 'No User found' });
+            return;
+        }
+        
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error finding specific user:', error);
+        res.status(500).json({ message: 'Internal Server Error' }); 
+    }
+}
+
+export {registerUser, loginUser, allUser, findUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import {registerUser, loginUser, allUser, findUser} from '../controllers/user.controllers.js'
+import verifyToken from '../middlewares/verifyToken.js'
 
 const router = express.Router()
-import {registerUser, loginUser} from '../controllers/user.controllers.js'
-
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+router.get('/all', verifyToken, allUser)
+router.get('/', verifyToken, findUser)
+
 
 export default router
