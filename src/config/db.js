7e3d34d86c249f54cfea09c5eee599d6e5dbe4d1
Chetan3327 <chauhanchetan12789@gmail.com>
2024-02-04 20:24:44 +0530

    real time messages

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connect = async () => {
     try {
         const conn = await mongoose.connect(process.env.DATABASE_URL)
-        console.log('connected')
+        console.log('DB connected')
     } catch (error) {
         console.log(error.message)
     }
--- src/server.js
+++ src/server.js
@@ -23,7 +23,48 @@ app.use('/api/chat', verifyToken, chatRoute)
 import messageRoute from './routes/message.routes.js'
 app.use('/api/message', verifyToken, messageRoute)
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log(`server started at port: ${PORT}`)
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://127.0.0.1:5173'
+    }
 })
+
+io.on('connection', (socket) => {
+    console.log('someone connected!');
+
+    socket.on('setup', (userData) => {
+        if(!userData) return
+        socket.join(userData._id)
+        console.log('object')
+        socket.emit('connected')
+    })
+
+    socket.on('join chat', (room) => {
+        socket.join(room)
+        console.log('joined room', room)
+    })
+
+    socket.on('new message', (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat
+        
+        if(!chat.users) return console.log('chat.users not defined')
+
+        chat.users.forEach((user) => {
+            if(user._id === newMessageRecieved.sender._id) return
+
+            socket.in(user._id).emit('message recieved', newMessageRecieved)
+        })
+    })
+});
+
+const PORT = process.env.PORT || 4000
+httpServer.listen(PORT, () => {
+    console.log(`server started at port ${PORT}`)
+});
